354987b113201a7fc28cbd359191392915931bcc
David Sharpe <david.a.sharpe@cgi.com>
2021-02-25 15:51:24 -0800

    Add note regarding nohup and sleep.

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 nohup /etc/alternatives/jre_11/bin/java -jar demo-0.0.1-SNAPSHOT.jar > nohup.out &
+# Without this sleep, the Jenkins SSH Steps plugin closes the connection before the nohup can take over?
 sleep 5
